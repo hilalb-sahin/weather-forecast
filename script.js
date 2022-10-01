5c9bde04df1de2b7646c59612fe61ed4d9856574
hilalb-sahin <114245624+hilalb-sahin@users.noreply.github.com>
2022-10-01 08:26:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
 var city = "london"; 
 
-
-
+//use your own api key
+//change city variable to view different locations
 
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city +"&APPID=YOUR_API_KEY",function(data){
-
-
-
-
-
-    console.log(data);
+    
     var location = data.name;
-    console.log(data.name);
+
     var weatherDescription = data.weather[0].description;
     var temperature = data.main.temp;
     var icon = "http://api.openweathermap.org/img/w/" +data.weather[0].icon +".png";
-    console.log(icon)
     $('.icon').attr('src',icon);
     var temperature = Math.floor(temperature- 273.15);
     $('.temp').append(temperature);
     $('.location').append(location);
-
-    console.log(data.getDate)
-    console.log(data.dt);
-    console.log(data.timezone);
     var time = new Date(data.dt*1000 +(data.timezone*1000)); 
-    console.log(time)
     $('.time').append(time);
-
-    console.log(weatherDescription)
     $('.weatherDescription').append(weatherDescription)
 
-    console.log(typeof(time));
 
     let time_stringified = JSON.stringify(time);
     console.log(time_stringified.slice(12,14))
-    //"2022-09-30T16:56:21.000Z"
     console.log(time_stringified);
     let time_hour = time_stringified.slice(12,14);
 
@@ -47,8 +32,15 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city +"&APPID=YOU
     } else {
         $('.greet').append('Good Afternoon 🌃')
     }
-
-
+    
+    //console.log(data);
+    //console.log(time)
+    //console.log(data.getDate)
+    //console.log(data.dt);
+    //console.log(data.timezone);
+    //console.log(weatherDescription);
+    //console.log(data.name);
+    //"2022-09-30T16:56:21.000Z"
 
 
 }
